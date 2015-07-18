6e31f62fb7062a5a4583ffddf91d66e164bbc110
David Wenzel <codenull@gmail.com>
2015-07-17 21:28:40 -0700

    Built skeleton of main activity. ListView now displays a text field, and a thumbnail image.

---
+++ app/src/main/java/com/vaporware/photohunt/CustomList.java
@@ -0,0 +1,42 @@
+package com.vaporware.photohunt;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * This is copyPasta from the internet.
+ * Mostly because I couldn't see any way to improve it,
+ * it is a pretty simple piece of code
+ *
+ */
+public class CustomList extends ArrayAdapter<String>{
+
+    private final Activity context;
+    private final String[] text;
+    private final Integer[] imageId;
+
+    public CustomList(Activity context, String[] text, Integer[] imageId) {
+        super(context, R.layout.list_item_site, text);
+        this.context = context;
+        this.text = text;
+        this.imageId = imageId;
+
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.list_item_site, null, true);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.list_item_site_textview);
+
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.list_item_site_imageview);
+        txtTitle.setText(text[position]);
+
+        imageView.setImageResource(imageId[position]);
+        return rowView;
+    }
+}
--- app/src/main/java/com/vaporware/photohunt/MainActivityFragment.java
+++ app/src/main/java/com/vaporware/photohunt/MainActivityFragment.java
@@ -5,12 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.widget.Toast;
 
 
 /**
@@ -23,26 +21,31 @@ public class MainActivityFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        Integer[] tempNum = {
+                0,
+                1,
+                2
+        };
 
         String[] tempArray = {
                 "words",
                 "are",
                 "hard"
         };
-        List<String> sitesInArea = new ArrayList<String>(Arrays.asList(tempArray));
 
-        mListAdapter = new ArrayAdapter<>(
-                getActivity(),
-                R.layout.list_item_site,
-                R.id.list_item_site_textview,
-                sitesInArea
-        );
-        ListView listview = (ListView) rootView.findViewById(R.id.mainList);
-        listview.setAdapter(mListAdapter);
-        //TODO: add setOnItemClickListener
+        CustomList adapter = new CustomList(getActivity(),tempArray,tempNum);
+        ListView list = (ListView) rootView.findViewById(R.id.mainList);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(),"You touched something",Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
         return rootView;
     }
